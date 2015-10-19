da5599a73b49eeb67627ff0bb37e74ad7294d53c
ANDY <ZaruZoba@zaruzoba.local>
2015-10-19 16:56:17 +0700

    EiEi

--- BlockThing/CircleMonster.swift
+++ BlockThing/CircleMonster.swift
@@ -47,3 +47,21 @@ class TriangleMonster: Monster {
 //        self.physicsBody?.applyForce(CGVectorMake(100.0, 0.0))
     }
 }
+class CircleMonster: Monster {
+    
+    override init(imageNamed: String, inX: Int, inY: Int) {
+        super.init(imageNamed: imageNamed, , )
+        
+        var body:SKPhysicsBody = SKPhysicsBody(circleOfRadius: (imageTexture.size().width/2))
+        body.dynamic = false
+        body.affectedByGravity = false
+        body.allowsRotation = false
+        body.categoryBitMask = BodyType.hero.rawValue
+        body.contactTestBitMask = BodyType.monster.rawValue
+        body.collisionBitMask = 0
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
