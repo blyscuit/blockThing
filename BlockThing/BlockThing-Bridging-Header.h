1fcb88e3513ecee77d92ef77179abe979a67f958
Bliss <blysxaye@icloud.com>
2015-11-11 10:20:09 +0700

    stageSelect

---
+++ BlockThing/BlockThing-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  BlockThing-Bridging-Header.h
+//  BlockThing
+//
+//  Created by Bliss Watchaye on 2015-11-11.
+//  Copyright © 2015 confusians. All rights reserved.
+//
+
+#import "M13ProgressViewSegmentedBar.h"
+#import "M13ProgressViewBar.h"
+#import "M13ProgressView.h"
--- BlockThing/StageSelectViewController.swift
+++ BlockThing/StageSelectViewController.swift
@@ -12,5 +12,24 @@ import UIKit
     func stageDidChoose(i:Int)
 }
 class StageSelectViewController: UIViewController {
+    
+    @IBOutlet weak var pro: M13ProgressViewBar!
     var delegate:StageSelectControllerDelegate?
+    
+    override func viewDidLoad() {
+//        var pro = M13ProgressViewBar()
+
+        pro.setIndeterminate(true)
+//        pro.numberOfSegments = 11
+//        pro.cornerRadius = pro.frame.size.width/CGFloat(pro.numberOfSegments)
+//        pro.segmentShape = M13ProgressViewSegmentedBarSegmentShapeRoundedRect
+//        pro.primaryColors = [UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor(),UIColor.darkGrayColor()]
+        //        pro.segmentSeparation = 1
+        pro.setPrimaryColor(UIColor.darkGrayColor())
+        pro.setSecondaryColor(UIColor.lightGrayColor())
+        pro.showPercentage = false
+        pro.progressBarThickness = 1
+//        progressView.addSubview(pro)
+//        progressView.backgroundColor = UIColor.clearColor()
+    }
 }
